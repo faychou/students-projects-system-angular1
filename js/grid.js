d2a676bd48c13fbddf224a9341947ae45e38161b
faychou <fay_chou@163.com>
2017-04-02 23:30:42 +0800

    添加导航滚动特效，设置底部背景

---
+++ js/grid.js
@@ -0,0 +1,20 @@
+// 小屏幕设备下，点击导航，自动收起导航条
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// 设置导航离开顶部特效
+$('#navbar').affix({
+    offset: {
+        top: 1
+    }
+});
+
+// 动态更换logo
+$(window).scroll(function() {
+    if ($('#navbar').hasClass('affix')) {
+        $('.logo').attr('src', 'image/com/bf-logo.png');
+    } else {
+        $('.logo').attr('src', 'image/com/bf-w-logo.png');
+    }
+});
